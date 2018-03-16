44bb6609661e1aef7ba3fbdacdc2a99a74619595
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-16 13:06:40 -0600

    ...

--- src/moosetabs/moosetabs.js
+++ src/moosetabs/moosetabs.js
@@ -26,7 +26,6 @@ export default class MooseTabs extends React.Component {
     }
 
     calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        debugger
         directionsService.route({
           origin: {lat:pos.lat, lng:pos.lng},
           destination: document.getElementById('end').value,
@@ -50,54 +49,58 @@ export default class MooseTabs extends React.Component {
     calculateRoute() {
         // this.directionsDisplay.setMap(map);
         // this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay)
-        debugger
-       var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: { lat: 45.676998, lng: -111.042931 }
-          });
-
-            infoWindow = new google.maps.InfoWindow();
-
-            if (navigator.geolocation) {
+        return new Promise((resolve, reject)=>{
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: { lat: 45.676998, lng: -111.042931 }
+              });
+    
+                infoWindow = new google.maps.InfoWindow();
+    
+                if (navigator.geolocation) {
+                    debugger
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        infoWindow.setPosition(pos);
+                        infoWindow.setContent('Your Are Here');
+                        infoWindow.open(map);
+                        map.setCenter(pos);
+                    }, function () {
+                        handleLocationError(true, infoWindow, map.getCenter());
+                    });
+                } else {
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, infoWindow, map.getCenter());
+                }
                 debugger
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
+                function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                     infoWindow.setPosition(pos);
-                    infoWindow.setContent('Your Are Here');
+                    infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
                     infoWindow.open(map);
-                    map.setCenter(pos);
-                }, function () {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                });
-            } else {
-                // Browser doesn't support Geolocation
-                handleLocationError(false, infoWindow, map.getCenter());
-            }
-            debugger
-            function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-                infoWindow.setPosition(pos);
-                infoWindow.setContent(browserHasGeolocation ?
-                    'Error: The Geolocation service failed.' :
-                    'Error: Your browser doesn\'t support geolocation.');
-                infoWindow.open(map);
-            }
+                }
+        }).then(()=>{
             this.directionsDisplay.setMap(map);
             this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay)
+
+        })
+       
             
         }
     
 
     render() {
 
-        navigator.geolocation.getCurrentPosition(function (position) {
-            pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-        })
+        // navigator.geolocation.getCurrentPosition(function (position) {
+        //     pos = {
+        //         lat: position.coords.latitude,
+        //         lng: position.coords.longitude
+        //     };
+        // })
 
         return (
             <div id="moosetabs-div">
